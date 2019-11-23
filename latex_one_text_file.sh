708924433e89061e85f7908383177aeef0a5bff9
Manos Athanassoulis <manos.athanassoulis@gmail.com>
2019-11-22 23:56:55 -0500

    working on the script

--- latex_one_text_file.sh
+++ latex_one_text_file.sh
@@ -1,7 +1,35 @@
 #! /bin/bash
 
+#Requires latexpand and delatex (opendetex)
 DETEX="delatex"
-FILE_NAME="$1"
+EXPAND="latexpand.pl"
+
+echo -n "Looking for latexpand ..."
+CHECK=`${EXPAND} --version`
+if [ $? -ne 0 ]; then
+	echo "Please install latexpand.pl in the path. https://gitlab.com/latexpand/latexpand"
+	exit
+fi
+echo " found!"
+
+echo -n "Looking for opendetex (2.8.5) ..."
+CHECK=`$DETEX -v | grep version | awk '{print $3}'`
+if [ "$CHECK" != "2.8.5" ]; then
+	echo "Please install opendetex 2.8.5. https://github.com/pkubowicz/opendetex"
+	exit
+fi
+echo " found!"
+
+FULL_PATH_FILE_NAME="$1"
+INPUT_DIR=`dirname $FULL_PATH_FILE_NAME`
+FILE_NAME=`basename $FULL_PATH_FILE_NAME`
+
+if [ "$INPUT_DIR" != "." ]; then
+	#we have to cd to input dir and get rid of the dir from path
+	echo "Moving to $INPUT_DIR ..."
+	cd $INPUT_DIR
+fi
+
 NO_EXTENSION_FILE=$(echo "$FILE_NAME" | cut -f 1 -d '.')
 
 if [ $# -eq 0 ]; then
@@ -14,11 +42,29 @@ FILE_PREPROCESSING=`mktemp latex_one_file_B_prepr.XXXXXXX`
 FILE_NO_LATEX=`mktemp latex_one_file_C_no_latex.XXXXXXX`
 FILE_TEXT_FINAL=`mktemp latex_one_file_D_final.XXXXXXX`
 
-latexpand.pl $FILE_NAME > $FILE_EXPANDED
+${EXPAND} $FILE_NAME > $FILE_EXPANDED
 
 
 echo "Ensuring that Introduction and Paragraph have a new line in $FILE_EXPANDED ..."
-cat ${FILE_EXPANDED} | sed '/\\section{.*}/{G;}'  | sed '/\\Paragraph{.*}/{G;}' | sed 's/\\numberofauthors{.*}//' | sed 's/ \\cite{.*}//g' | sed 's/~\\cite{.*}//g' | sed 's/\\bibliographystyle{.*}//' | sed 's/\\begin{thebibliography}{.*}//' > ${FILE_PREPROCESSING}
+# cat ${FILE_EXPANDED} | sed '/\\section{.*}/{G;}'  | sed '/\\Paragraph{.*}/{G;}' | sed 's/\\numberofauthors{.*}//' | sed 's/ \\cite{.*}//g' | sed 's/~\\cite{.*}//g' | sed 's/\\bibliographystyle{.*}//' | sed 's/\\begin{thebibliography}{.*}//' > ${FILE_PREPROCESSING}
+cat ${FILE_EXPANDED} | sed '/\\section{.*}/{G;}'  | sed '/\\Paragraph{.*}/{G;}' > ${FILE_PREPROCESSING} 
+cat ${FILE_EXPANDED} > ${FILE_PREPROCESSING} 
+sed -i '' 's/\\numberofauthors{.*}//' ${FILE_PREPROCESSING} 
+sed -i '' 's/ \\cite{.*}//g' ${FILE_PREPROCESSING} 
+sed -i '' 's/~\\cite{.*}//g' ${FILE_PREPROCESSING} 
+sed -i '' 's/\\bibliographystyle{.*}//' ${FILE_PREPROCESSING} 
+sed -i '' 's/\\begin{thebibliography}{.*}//' ${FILE_PREPROCESSING} 
+sed -i '' 's/\\acmConference{.*}{.*}//' ${FILE_PREPROCESSING} 
+sed -i '' 's/\\settopmatter{.*}//' ${FILE_PREPROCESSING} 
+sed -E -i '' 's/(\\Paragraph{.*)}/\1\.}/g' ${FILE_PREPROCESSING}
+sed -E -i '' 's/(\\section{.*)}/\1\.}/g' ${FILE_PREPROCESSING}
+sed -E -i '' 's/(\\subsection{.*)}/\1\.}\\n/g' ${FILE_PREPROCESSING}
+sed -E -i '' 's/(\\subsubsection{.*)}/\1\.}\\n/g' ${FILE_PREPROCESSING}
+sed -E -i '' 's/\$([0-9]*)\$/\1/g' ${FILE_PREPROCESSING}
+sed -E -i '' 's/\$([0-9\.]*\\%)\$/\1/g' ${FILE_PREPROCESSING}
+sed -E -i '' 's/\$([a-zA-Z]*\\%)\$/\1/g' ${FILE_PREPROCESSING}
+sed -E -i '' 's/(\\begin{.*})\[.*\]/\1/g' ${FILE_PREPROCESSING}
+
 echo "======================="
 
 #replace simple macros ?
@@ -35,13 +81,6 @@ sed -i '' 's/\\comp{}/FADE/g' ${FILE_PREPROCESSING}
 sed -i '' 's/\\comp/FADE/g' ${FILE_PREPROCESSING}
 sed -i '' 's/\\dpl{}/DPL/g' ${FILE_PREPROCESSING}
 sed -i '' 's/\\dpl/DPL/g' ${FILE_PREPROCESSING}
-sed -E -i '' 's/(\\Paragraph{.*)}/\1\.}/g' ${FILE_PREPROCESSING}
-sed -E -i '' 's/(\\section{.*)}/\1\.}/g' ${FILE_PREPROCESSING}
-sed -E -i '' 's/(\\subsection{.*)}/\1\.}/g' ${FILE_PREPROCESSING}
-sed -E -i '' 's/(\\subsubsection{.*)}/\1\.}/g' ${FILE_PREPROCESSING}
-sed -E -i '' 's/\$([0-9]*)\$/\1 /g' ${FILE_PREPROCESSING}
-sed -E -i '' 's/\$([0-9\.]*\\%)\$/\1 /g' ${FILE_PREPROCESSING}
-sed -E -i '' 's/\$([a-zA-Z]*\\%)\$/\1 /g' ${FILE_PREPROCESSING}
 echo "======================="
 
 echo "Delatex..."
@@ -63,3 +102,54 @@ rm $FILE_PREPROCESSING
 rm $FILE_NO_LATEX
 rm $FILE_TEXT_FINAL
 
+exit
+
+echo "Entering \"staging\" directory ..."
+cd staging
+
+FILE_NAME="$1"
+NO_EXTENSION_FILE=$(echo "$FILE_NAME" | cut -f 1 -d '.')
+
+FILE_NAME_STEP_1="${NO_EXTENSION_FILE}-step1.tex"
+FILE_NAME_STEP_2="${NO_EXTENSION_FILE}-step2.tex"
+NO_TEX_FILE="${NO_EXTENSION_FILE}.txt"
+FIXED_FILE="${NO_EXTENSION_FILE}-pure.txt"
+
+echo "Latex expand $1 ..."
+if [ -f ${NO_EXTENSION_FILE}.bbl ]; then
+	echo "(Trying to expand bbl as well)"
+	LATEXPAND="${LATEXPAND} --expand-bbl ${NO_EXTENSION_FILE}.bbl"
+fi	
+${LATEXPAND} ${FILE_NAME} > ${FILE_NAME_STEP_1} 
+echo
+
+# cat $FILE_NAME_STEP_1
+# exit
+
+echo "Ensuring that Introduction and Paragraph have a new line in $FILE_NAME_STEP_1 ..."
+cat ${FILE_NAME_STEP_1} | sed '/\\section{.*}/{G;}'  | sed '/\\Paragraph{.*}/{G;}' | sed 's/\\numberofauthors{.*}//' | sed 's/ \\cite{.*}//g' | sed 's/~\\cite{.*}//g' | sed 's/\\bibliographystyle{.*}//' | sed 's/\\begin{thebibliography}{.*}//' > ${FILE_NAME_STEP_2}
+echo
+
+# cat ${FILE_NAME_STEP_2}
+# exit
+
+echo "Detexing $FILE_NAME_STEP_2 ..."
+${DETEX} -n ${FILE_NAME_STEP_2} > ${NO_TEX_FILE}
+echo
+
+echo "Dos2Unix ${NO_TEX_FILE} ..."
+dos2unix ${NO_TEX_FILE}
+echo
+
+# cat ${NO_TEX_FILE}
+# exit 
+
+echo "Fixing new lines and saving to $FIXED_FILE ... "
+cat ${NO_TEX_FILE} | awk '/^$/ { print "\n"; } /./ { printf("%s ", $0); } END { print ""; }' | sed -n 's/ \+/ /gp' > $FIXED_FILE
+echo
+
+#cat $FIXED_FILE
+
+rm -rf $FILE_NAME_STEP_1 $FILE_NAME_STEP_2 $NO_TEX_FILE
+
+cp $FIXED_FILE ../latest-pure-text.txt
